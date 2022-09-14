704eb75e887d7e50f38a51ba1e117accde7db46f
“juanP1996” <parrajp1996@gmail.com>
2022-09-14 18:07:48 +0200

    feat: pokemones traidos

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -5,51 +5,54 @@ import axios from 'axios';
 
 const Home = () => {
 
-    const [pokemon, setPokemon] = useState({
-        name: "", img: ""
-    });
+    const [pokemones, setPokemones] = useState([]);
 
     useEffect(() => {
-
-
-        // traer();
+        traer();
     }, []);
-
 
     const traer = () => {
 
         try {
 
-            axios.get("https://pokeapi.co/api/v2/pokemon/charmander").then(
-                (response) => {
-                    setPokemon({ name: response.data.name, img: response.data.sprites.front_default });
-
-                }
-            )
-
+            axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+                .then(resp => {
+                    for (let i = 0; i < resp.data.results.length; i++) {
+                        axios.get(resp.data.results[i].url)
+                            .then(result => {
+                                setPokemones(prevArray => [...prevArray, result.data])
+                                console.log(result.data)
+                            })
+                    }
+                })
         } catch (error) {
             console.log(error);
         }
-
 
     }
     return (
         <div className='home'>
 
-            <div className="lado_der">
-                <>
-            {
-                pokemon.name
-            }
-                <img src={pokemon.img} />
-                
-                </>
-
-            <div id="boton" onClick={() => traer()}>traer</div>
-
-            </div>
-            
             <div className="lado_izq">
+                {
+                    pokemones.map((datica) => {
+                        return (
+                            <div className='cartas' key={datica.id}>
+                                nombre:{datica.name} <br />
+                                elemento:{datica.types[0].type.name} <br />
+                                ataque:{datica.stats[1].base_stat} <br />
+                                ataque especial:{datica.stats[3].base_stat} <br />
+                                vida:{datica.stats[0].base_stat} <br />
+                                velocidad:{datica.stats[5].base_stat} <br />
+                                defensa:{datica.stats[2].base_stat} <br />
+                                <img src={datica.sprites.front_default}></img>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div className="lado_der">
 
                 que fue
             </div>
